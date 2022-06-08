dc88f6793dad6d34c26db4aadb07e6db86d68af8
Warren <hello@warren.codes>
2022-06-08 09:39:14 -0600

    Add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Example</title>
+</head>
+<body>
+    <h1>PHP Example</h1>
+    <p>
+        5 + 5 =
+        <?php echo 5 + 5; ?>
+    </p>
+</body>
+</html>
