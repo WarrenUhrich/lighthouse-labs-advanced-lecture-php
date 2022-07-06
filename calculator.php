6834adbb7067d1e32ac3163fda65e46e6adb7b92
Warren <hello@warren.codes>
2022-07-06 10:15:26 -0600

    Add sum calculator example.

---
+++ calculator.php
@@ -0,0 +1,30 @@
+<?php include './templates/head.php'; ?>
+
+    <?php include './templates/navigation.php'; ?>
+
+    <p>
+        Please enter your numbers below:
+    </p>
+
+    <pre>
+        <?php var_dump($_POST); ?>
+    </pre>
+
+    <form action="/calculator.php" method="POST">
+        <label for="num1">First Number:</label>
+        <input id="num1" type="number" name="number1">
+        <label for="num2">Second Number:</label>
+        <input id="num2" type="number" name="number2">
+        <input type="submit" value="Calculate Sum">
+        <?php if (!empty($_POST)) { ?>
+            <p>
+                <?php echo $_POST['number1']; ?>
+                +
+                <?php echo $_POST['number2']; ?>
+                =
+                <?php echo $_POST['number1'] + $_POST['number2']; ?>
+            </p>
+        <?php } // End empty $_POST if. ?>
+    </form>
+
+<?php include './templates/footer.php'; ?>
