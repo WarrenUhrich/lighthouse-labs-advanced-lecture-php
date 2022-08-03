099520bc144e4857848612072fc55287e099a6cc
Warren <hello@warren.codes>
2022-08-03 09:55:26 -0600

    Add includes / navigation example

---
+++ templates/navigation.php
@@ -0,0 +1,11 @@
+<nav>
+    <h2>Web Site Navigation</h2>
+    <ul>
+        <li>
+            <a href="/index.php">Index</a>
+        </li>
+        <li>
+            <a href="/about.php">About</a>
+        </li>
+    </ul>
+</nav>
